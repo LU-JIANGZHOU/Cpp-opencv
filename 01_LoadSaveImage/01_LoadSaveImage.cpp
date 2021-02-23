822206064109cf5978879e49b899475c6700705b
LU-JIANGZHOU <jiangzhoulu@outlook.com>
2021-02-23 23:49:09 +0800

    feb23

--- 01_LoadSaveImage/01_LoadSaveImage.cpp
+++ 01_LoadSaveImage/01_LoadSaveImage.cpp
@@ -11,8 +11,11 @@ using namespace cv;
 int main()
 {
 	//Mat acesColor= imread("aces.png", IMREAD_COLOR);
-	Mat acesColor= imread("aces.png", IMREAD_UNCHANGED);
-	Mat acesGrey=imread("aces.png", IMREAD_GRAYSCALE);
+	//Mat acesColor= imread("aces.png", IMREAD_UNCHANGED);
+	//Mat acesGrey=imread("aces.png", IMREAD_GRAYSCALE);
+
+	Mat acesColor= imread("aces.png", -1);
+	Mat acesGrey=imread("aces.png", 0);
 
 	imshow("Aces Color", acesColor);
 	imshow("Aces Grey", acesGrey);

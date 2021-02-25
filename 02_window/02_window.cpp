836f9ad02efef76cfa7ace4995d304064a303560
LU-JIANGZHOU <jiangzhoulu@outlook.com>
2021-02-25 23:18:37 +0800

    Feb 25

--- 02_window/02_window.cpp
+++ 02_window/02_window.cpp
@@ -7,9 +7,27 @@
 #include <opencv2/highgui.hpp>
 
 using namespace std;
-
+using namespace cv;
 
 int main()
 {
+	Mat acesColor = imread("aces.png", -1);
+	Mat acesGrey = imread("aces.png", 0);
+
+	namedWindow("Color", WINDOW_FREERATIO);
+	namedWindow("Fixed", WINDOW_AUTOSIZE);
+
+	imshow("Color", acesColor);
+	imshow("Fixed", acesGrey);
+
+	resizeWindow("Color", acesColor.cols / 4, acesColor.rows / 4);
+	resizeWindow("Fixed", acesGrey.cols / 4, acesGrey.rows / 4);
+	//imwrite("acesColor.jpg", acesColor);
+	//imwrite("acesGrey.jpg", acesGrey);
+
+	moveWindow("Color", 20, 20);
+	moveWindow("fixed", 20+acesColor.cols, 20);
+	waitKey();
+
 	return EXIT_SUCCESS;
 }
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -46,6 +46,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/lujiangzhou/Desktop/CppWorkSpace/Cpp-opencv/build/Core_Mat/cmake_install.cmake")
   include("/home/lujiangzhou/Desktop/CppWorkSpace/Cpp-opencv/build/01_LoadSaveImage/cmake_install.cmake")
+  include("/home/lujiangzhou/Desktop/CppWorkSpace/Cpp-opencv/build/02_window/cmake_install.cmake")
 
 endif()
 

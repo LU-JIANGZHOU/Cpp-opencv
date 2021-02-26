3a64de36c7f88eddf3e57d8840d27733c19a9dc0
LU-JIANGZHOU <jiangzhoulu@outlook.com>
2021-02-26 23:27:17 +0800

    Feb 26

--- 02_window/02_window.cpp
+++ 02_window/02_window.cpp
@@ -11,22 +11,22 @@ using namespace cv;
 
 int main()
 {
-	Mat acesColor = imread("aces.png", -1);
-	Mat acesGrey = imread("aces.png", 0);
+	Mat focalFossaC = imread("FocalFossa.jpg", 1);
+	Mat focalFossaG = imread("FocalFossa.jpg", 0);
 
 	namedWindow("Color", WINDOW_FREERATIO);
 	namedWindow("Fixed", WINDOW_AUTOSIZE);
 
-	imshow("Color", acesColor);
-	imshow("Fixed", acesGrey);
+	imshow("Color", focalFossaC);
+	imshow("Fixed", focalFossaG);
 
-	resizeWindow("Color", acesColor.cols / 4, acesColor.rows / 4);
-	resizeWindow("Fixed", acesGrey.cols / 4, acesGrey.rows / 4);
+	resizeWindow("Color", focalFossaC.cols , focalFossaC.rows );
+	resizeWindow("Fixed", focalFossaG.cols , focalFossaG.rows );
 	//imwrite("acesColor.jpg", acesColor);
 	//imwrite("acesGrey.jpg", acesGrey);
 
 	moveWindow("Color", 20, 20);
-	moveWindow("fixed", 20+acesColor.cols, 20);
+	moveWindow("fixed", 20+focalFossaC.cols, 20);
 	waitKey();
 
 	return EXIT_SUCCESS;
